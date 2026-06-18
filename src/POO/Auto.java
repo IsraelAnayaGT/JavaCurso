3891b282cac42a6e8c78c45e72c7b7f2f36a2c5a
Vikingo <israelanayaabc@gmail.com>
2026-06-18 14:49:26 -0600

    Sobre carga de metodos #48

---
+++ src/POO/Auto.java
@@ -0,0 +1,36 @@
+package POO;
+
+ class Auto {
+    // Sobre carga de metodos con constructores
+    String marca , modelo;
+    int ano;
+
+    //Metodo 1 sin parametros
+    public Auto(){
+        marca = "Ford";
+        modelo = "Fiesta"; 
+        ano = 2010;
+    }
+
+
+    //Metodo 2 con un parametro
+    public Auto(String marca){
+        this.marca = marca;
+        this.modelo = "Matrix";
+        this.ano = 2010;
+    }
+
+    void MostrarInfo(){
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+    
+    public static void main(String[] args){
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto("Chevrolet");
+        auto1.MostrarInfo();
+        auto2.MostrarInfo();
+
+    }
+}
