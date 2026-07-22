1566771f64add53200e4c9d67553c9b0f833987e
El Vikingo <57925478+IsraelAnayaGT@users.noreply.github.com>
2026-07-22 11:51:41 -0600

    #52 Encapsulacion, modificador public

---
+++ src/POO_Encapsulacion/Principal.java
@@ -0,0 +1,10 @@
+package POO_Encapsulacion;
+import POO_Encapsulacion.prueba.Persona;
+
+public class Principal {
+    public static void main(String[] args) {
+    Persona persona1 = new Persona();
+    persona1.Dimenombre();
+    
+    }
+}
---
+++ src/POO_Encapsulacion/prueba/Persona.java
@@ -0,0 +1,10 @@
+package POO_Encapsulacion.prueba;
+
+
+public class Persona {
+    String nombre = "Israel";
+    
+    public void Dimenombre(){
+        System.out.println(nombre);
+    }
+}
