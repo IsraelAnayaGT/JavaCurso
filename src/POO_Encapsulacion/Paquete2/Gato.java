4004941e33a2ebcc71a85a9eff6b2c506d65c128
El Vikingo <57925478+IsraelAnayaGT@users.noreply.github.com>
2026-07-22 12:32:30 -0600

    #53 Encapsulacion, modificador protected

---
+++ src/POO_Encapsulacion/Paquete2/Gato.java
@@ -0,0 +1,11 @@
+package POO_Encapsulacion.Paquete2;
+import POO_Encapsulacion.Paquete1.Animal;
+
+public class Gato {
+    public static void main(String[] args){
+        Animal gato1 = new Animal();
+        gato1.maulla();
+        
+        System.out.println("El nombre del gato es:");
+    }
+}
