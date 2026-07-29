e86a0e2e7b8d4a412f21942f0118a3d9bee859a9
El Vikingo <57925478+IsraelAnayaGT@users.noreply.github.com>
2026-07-29 09:45:50 -0600

    #53 Se añadio paquete 1 y archivo anima.

---
+++ src/POO_Encapsulacion/Paquete1/Animal.java
@@ -0,0 +1,14 @@
+package POO_Encapsulacion.Paquete1;
+
+public class Animal {
+    public String nombre = "Pelusa";
+
+    protected void caminar(){
+        System.out.println("El animal camina");
+    }
+
+    public void maulla(){
+        System.out.println("Miau miau");
+    }
+    
+}
